5916d4205b97e123e9e7cac7f2cb0025d00f0b78
zqh-china <zhangqihao628@gmail.com>
2024-12-11 02:11:51 +0800

    about页使用动态数据

--- src/api/index.js
+++ src/api/index.js
@@ -1,29 +1,36 @@
 import axios from 'axios';
 const baseURL = 'http://localhost:3000';
+// const headers = {
+//     'Content-Type': 'application/json',
+//     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwidXNlcmlkIjoxLCJpYXQiOjE3MzM2MzA5MzgsImV4cCI6NDMyNTYzMDkzOH0.x7oNAVuAdpk3QO10Uu48GnAWJhZPEeNa8tZF2Zwt6F8',
+// }
 const headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwidXNlcmlkIjoxLCJpYXQiOjE3MzM2MzA5MzgsImV4cCI6NDMyNTYzMDkzOH0.x7oNAVuAdpk3QO10Uu48GnAWJhZPEeNa8tZF2Zwt6F8',
 }
 export function getAllTags() {
-    return axios.get(`${baseURL}/tag`, { headers })
+    return axios.get(`${baseURL}/tag`, { headers });
 }
 
 export function getAllCategories() {
-    return axios.get(`${baseURL}/category`, { headers })
+    return axios.get(`${baseURL}/category`, { headers });
 }
 
 export function getAllArticles() {
-    return axios.get(`${baseURL}/article`, { headers })
+    return axios.get(`${baseURL}/article`, { headers });
 }
 
 export function getArticle(id) {
-    return axios.get(`${baseURL}/article/${id}`, { headers })
+    return axios.get(`${baseURL}/article/${id}`, { headers });
 }
 
 export function getArticleParagraphs(id) {
-    return axios.get(`${baseURL}/paragraph/${id}`, { headers })
+    return axios.get(`${baseURL}/paragraph/${id}`, { headers });
 }
 
 export function getAuthor(id) {
-    return axios.get(`${baseURL}/user/${id}`, { headers })
+    return axios.get(`${baseURL}/user/${id}`, { headers });
+}
+
+export function getChangelog() {
+    return axios.get(`${baseURL}/changelog`, { headers });
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router';
 import Home from '../views/Home.vue';
 import ArticleDetail from '../views/ArticleDetail.vue';
 import BasicLayout from '../views/BasicLayout.vue';
@@ -35,7 +35,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 });
 
